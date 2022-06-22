6ef36ee34dce8c677ee2d04755bb65ac52b5c45e
Vincent Caronnet <120@120.design>
2022-06-23 01:09:52 +0200

    Corrections mineures dans le HomeSentencesComponent

--- components/HomeComponent/HomeSentencesComponent.tsx
+++ components/HomeComponent/HomeSentencesComponent.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { FC, useEffect, useRef, useState } from 'react'
 
-interface PropsInterface {
+interface Props {
     sentences: JSX.Element[]
 }
 
-const HomeSentencesComponent: FC<PropsInterface> = ({ sentences }) => {
+const HomeSentencesComponent: FC<Props> = ({ sentences }) => {
     const [randomSentence, setRandomSentence] = useState(<></>)
 
     const lastRandomSentence = useRef<JSX.Element>(<></>)
 
     useEffect(() => {
         const interval = setInterval(() => {
-            let filteredSentences = sentences.filter(
+            const filteredSentences = sentences.filter(
                 s => s !== lastRandomSentence.current
             )
 

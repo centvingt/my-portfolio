ecbceae8c88721c395e95247f5de5eb98effad53
Vincent Caronnet <120@120.design>
2022-06-23 17:34:11 +0200

    Ajout du meta:vp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import LayoutComponent from '../components/LayoutComponent'
 
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <LayoutComponent>
-            <Component {...pageProps} />
-        </LayoutComponent>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <LayoutComponent>
+                <Component {...pageProps} />
+            </LayoutComponent>
+        </>
     )
 }
 

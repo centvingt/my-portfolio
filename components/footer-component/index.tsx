114f6bc2d682b345e0c06a06a53c7740c839f284
Vincent Caronnet <120@120.design>
2022-06-30 20:39:22 +0200

    Ajout des URLs dans les boutons du footer

--- components/footer-component/index.tsx
+++ components/footer-component/index.tsx
@@ -1,19 +1,36 @@
 import { FC } from 'react'
 import { BsInstagram, BsLinkedin, BsGithub, BsEnvelope } from 'react-icons/bs'
+import Link from 'next/link'
 
 const FooterComponent: FC = () => (
     <footer className="fixed bottom-6 right-6 grid gap-4">
         <div className="grid h-10 w-10 items-center justify-center bg-tonic text-slate-900">
-            <BsInstagram size={'28px'} />
+            <Link href="https://github.com/centvingt/">
+                <a target="_blank" rel="nofollow">
+                    <BsGithub size={'28px'} />
+                </a>
+            </Link>
         </div>
         <div className="grid h-10 w-10 items-center justify-center bg-tonic text-slate-900">
-            <BsLinkedin size={'28px'} />
+            <Link href="https://www.linkedin.com/in/120design/">
+                <a target="_blank" rel="nofollow">
+                    <BsLinkedin size={'28px'} />
+                </a>
+            </Link>
         </div>
         <div className="grid h-10 w-10 items-center justify-center bg-tonic text-slate-900">
-            <BsGithub size={'28px'} />
+            <Link href="https://www.instagram.com/apps.120design/">
+                <a target="_blank" rel="nofollow">
+                    <BsInstagram size={'28px'} />
+                </a>
+            </Link>
         </div>
         <div className="grid h-10 w-10 items-center justify-center bg-tonic text-slate-900">
-            <BsEnvelope size={'28px'} />
+            <Link href="/contact/">
+                <a>
+                    <BsEnvelope size={'28px'} />
+                </a>
+            </Link>
         </div>
     </footer>
 )

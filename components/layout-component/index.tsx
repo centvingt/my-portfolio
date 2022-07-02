598a59a289ba9795f47e2f8dc4263682c91dbda7
Vincent Caronnet <120@120.design>
2022-07-02 09:45:27 +0200

    La nav mobile est systématiquement refermée quand la page est agrandie si bien que si on réduit la page de nouveau elle n’apparaît plus ouverte mais refermée

--- components/layout-component/index.tsx
+++ components/layout-component/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useState } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
 import FooterComponent from '../footer-component'
 import NavComponent from '../nav-component'
 
@@ -8,6 +8,12 @@ interface Props {
 
 const LayoutComponent: FC<Props> = ({ children }) => {
     const [navIsClosed, setNavIsClosed] = useState(false)
+
+    useEffect(() => {
+        window
+            .matchMedia('(min-width: 640px)')
+            .addEventListener('change', () => setNavIsClosed(true))
+    }, [])
 
     return (
         <>

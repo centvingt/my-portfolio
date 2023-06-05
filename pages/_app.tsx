3f3dfda800eb714a712ed99dae931a7e3c97b285
Vincent Caronnet <120@120.design>
2023-06-05 16:27:28 +0200

    Ajout du script de Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import LayoutComponent from '@/components/layout-component'
+import Script from 'next/script'
 
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script strategy="lazyOnload" id="script">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
             <Head>
                 <meta
                     name="viewport"

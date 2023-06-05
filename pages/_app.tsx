2ce37ccbbd7089505d0bd12cf7fed3cfc9535184
Vincent Caronnet <120@120.design>
2023-06-05 16:58:54 +0200

    Modification de la balise script de Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,18 +9,16 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Script
-                strategy="lazyOnload"
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                strategy="afterInteractive"
             />
 
-            <Script strategy="lazyOnload" id="script">
+            <Script id="google-analytics" strategy="afterInteractive">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                 `}
             </Script>
 
